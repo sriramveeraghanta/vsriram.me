a93663906114921198573ea81639813369a8a7e1
sriramveeraghanta <veeraghanta.sriram@gmail.com>
2020-08-05 23:51:50 -0400

    bubble fix

--- layouts/BasicLayout.js
+++ layouts/BasicLayout.js
@@ -1,15 +1,43 @@
 import React from "react";
 // components
 import SidebarComponent from "components/page_elements/SidebarComponent";
+// framer
+import { motion } from "framer-motion";
 
 function BasicLayout(props) {
+  const { route } = props;
+
+  const vairants = {
+    pageInitial: {
+      opacity: 0.4,
+    },
+    pageAnimate: {
+      opacity: 1,
+    },
+    pageExit: {
+      backgroundColor: "white",
+      filter: `invert()`,
+      opacity: 0,
+    },
+  };
+
   return (
     <>
       <div className="page-layout">
         <aside className="site-sidebar">
           <SidebarComponent />
         </aside>
-        <main className="site-main">{props.children}</main>
+        <main className="site-main">
+          <motion.div
+            key={route}
+            initial="pageInitial"
+            animate="pageAnimate"
+            exit="pageExit"
+            variants={vairants}
+          >
+            {props.children}
+          </motion.div>
+        </main>
       </div>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -4,44 +4,140 @@ import Head from "next/head";
 // layouts
 import BasicLayout from "layouts/BasicLayout";
 // react bootstrap
-import { Container } from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
 
 function HomePage() {
+  const bubbleBackgroud = () => {
+    var canvas = document.getElementById("container");
+    var clone = document.getElementById("blurCanvasBottom");
+
+    var cloneCtx = clone.getContext("2d");
+    var ctx = canvas.getContext("2d");
+
+    var w = $("#blurCanvasTop").width();
+    var h = $("#blurCanvasTop").height();
+
+    var ww = $(window).width();
+    var wh = $(window).height();
+    canvas.width = ww;
+    canvas.height = wh;
+    var partCount = 100;
+    var particles = [];
+
+    function particle() {
+      this.color = "rgba(255,255,255," + Math.random() + ")";
+      // console.log(this.color);
+      this.x = randomInt(0, ww);
+      this.y = randomInt(0, wh);
+      this.direction = {
+        x: -1 + Math.random() * 2,
+        y: -1 + Math.random() * 2,
+      };
+      this.vx = 0.3 * Math.random();
+      this.vy = 0.3 * Math.random();
+      this.radius = randomInt(2, 3);
+      this.float = function () {
+        this.x += this.vx * this.direction.x;
+        this.y += this.vy * this.direction.y;
+      };
+      this.changeDirection = function (axis) {
+        this.direction[axis] *= -1;
+      };
+      this.boundaryCheck = function () {
+        if (this.x >= ww) {
+          this.x = ww;
+          this.changeDirection("x");
+        } else if (this.x <= 0) {
+          this.x = 0;
+          this.changeDirection("x");
+        }
+        if (this.y >= wh) {
+          this.y = wh;
+          this.changeDirection("y");
+        } else if (this.y <= 0) {
+          this.y = 0;
+          this.changeDirection("y");
+        }
+      };
+      this.draw = function () {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fill();
+      };
+    }
+    function clearCanvas() {
+      cloneCtx.clearRect(0, 0, ww, wh);
+      ctx.clearRect(0, 0, ww, wh);
+    }
+    function createParticles() {
+      for (let i = 0; i < partCount; i++) {
+        let p = new particle();
+        particles.push(p);
+      }
+    }
+    function drawParticles() {
+      for (let i = 0; i < particles.length; i++) {
+        let p = particles[i];
+        p.draw();
+      }
+    }
+    function updateParticles() {
+      for (let i = particles.length - 1; i >= 0; i--) {
+        let p = particles[i];
+        p.float();
+        p.boundaryCheck();
+      }
+    }
+    createParticles();
+    drawParticles();
+    function animateParticles() {
+      clearCanvas();
+      drawParticles();
+      updateParticles();
+      cloneCtx.drawImage(canvas, 0, 0);
+      requestAnimationFrame(animateParticles);
+    }
+    requestAnimationFrame(animateParticles);
+    cloneCtx.drawImage(canvas, 0, 0);
+
+    $(window).on("resize", function () {
+      ww = $(window).width();
+      wh = $(window).height();
+      canvas.width = ww;
+      canvas.height = wh;
+      clearCanvas();
+      particles = [];
+      createParticles();
+      drawParticles();
+    });
+    function randomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    function velocityInt(min, max) {
+      return Math.random() * (max - min + 1) + min;
+    }
+  };
+
+  React.useEffect(() => {
+    bubbleBackgroud();
+  });
+
   return (
     <>
       <Head>
         <title>Sriram Veeraghanta</title>
         <link rel="icon" href="/favicon.ico" />
-        <script src=""></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/snap.svg/0.3.0/snap.svg-min.js"></script>
-        <script src="/static/js/bubble-bg.js"></script>
       </Head>
       <div className="index-background">
         <canvas id="container" role="main"></canvas>
         <div className="content">
           <h1 className="title">Sriram Veeraghanta</h1>
           <p className="desc">Web Developer, Product Designer and Machine Learning Enthusiast.</p>
-          <ul className="contact">
-            <li>
-              <a href="mailto:jonas@badalic.com">jonas@badalic.com</a>
-            </li>
-            <li>
-              <a target="_top" href="https://codepen.io/JonasB/">
-                Codepen
-              </a>
-            </li>
-            <li>
-              <a target="_top" href="https://twitter.com/JonasBadalic">
-                Twitter
-              </a>
-            </li>
-            <li>
-              <a target="_top" href="https://si.linkedin.com/in/jonasbadalic">
-                LinkedIn
-              </a>
-            </li>
-          </ul>
+          <Button className="btn-boxed btn-white">Know More</Button>
         </div>
         <div className="blur blurTop">
           <canvas className="canvas" id="blurCanvasTop"></canvas>
