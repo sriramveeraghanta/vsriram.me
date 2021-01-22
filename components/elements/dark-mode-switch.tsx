2eefa5e746d344edd20fde7892732df0ea85a700
sriramveeraghanta <veeraghanta.sriram@gmail.com>
2021-01-21 20:22:33 -0500

    theme button fix

--- components/elements/dark-mode-switch.tsx
+++ components/elements/dark-mode-switch.tsx
@@ -1,25 +1,27 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 // react themes
 import { useTheme } from 'next-themes';
 // react icons
 import { FaSun, FaMoon } from 'react-icons/fa';
 
 const DarkModeSwitch: FC = () => {
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
+
+  // After mounting, we have access to the theme
+  useEffect(() => setMounted(true), []);
 
   return (
     <div className="switch-container">
       <div>
-        {theme === 'light' && (
-          <button className="btn btn-theme-light" onClick={() => setTheme('dark')}>
-            <FaMoon />
-          </button>
-        )}
-        {theme === 'dark' && (
-          <button className="btn btn-theme-dark" onClick={() => setTheme('light')}>
-            <FaSun />
-          </button>
-        )}
+        <button
+          aria-label="Toggle Dark Mode"
+          type="button"
+          className="btn btn-theme-light"
+          onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+        >
+          {mounted && theme === 'dark' ? <FaSun /> : <FaMoon />}
+        </button>
       </div>
     </div>
   );
