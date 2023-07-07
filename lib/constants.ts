b8d3b1f2368767574d0c3ff21310ca59d5a8cc10
sriramveeraghanta <veeraghanta.sriram@gmail.com>
2023-07-07 11:52:27 +0100

    bio change

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 export const GITHUB_USERNAME = "sriramveeraghanta";
 export const FULL_NAME = "Sriram Veeraghanta";
 export const ABOUT =
-  "Hey, I'm Sriram. I'm a Solution Designer at Deloitte working on Enterprise Transformation Solutions and Customer Experience.";
+  "Hey, I'm Sriram - a Software Architect and Engineering Manager, passionate about empowering businesses through transformative software solutions.";
 export const BIO =
-  "I am a full-stack architect/engineer building enterprise solutions using open-source technologies like Typescript, Python, Java, etc. Experienced in building interactive solutions in edTech, e-commerce, fintech and real-time communications. I build scalable and secure applications using microservices architectures and best practices.";
+  "Welcome to my personal website! I am a highly skilled Software Architect and Engineering Manager with expertise in solution design, technical leadership, and project management. With a strong background in web technologies and a passion for creating exceptional customer experience transformation, I have successfully designed and developed scalable applications using modern frameworks and technologies. My experience spans various industries, including fintech, ed-tech and e-commerce, where I have collaborated with clients, led teams, and implemented best practices to deliver high-quality solutions. On this website, you will find more information about my skills, experience, and projects. Feel free to explore and get in touch to discuss potential collaborations or opportunities. Let's build innovative and impactful software together!";
